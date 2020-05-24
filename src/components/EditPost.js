77afd54eaaec56139d2d67532665b01a83279353
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-05-24 19:04:59 +0900

    11

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -9,13 +9,39 @@ export default class EditPost extends Component {
 		postOwnerId: '',
 		postOwnerUsername: '',
 		postTitle: '',
-		postBody: ''
+		postBody: '',
+		postData: {
+			postTitle: this.props.postTitle,
+			postBody: this.props.postBody
+		}
 	};
 
 	handleModal = () => {
 		this.setState({ show: !this.state.show });
 		document.body.scrollTop = 0;
 		document.documentElement.scrollTop = 0;
+	};
+
+	handleUpdatePost = async (event) => {
+		event;
+	};
+
+	handleTitle = (event) => {
+		this.setState({
+			postData: {
+				...this.state.postData,
+				postTitle: event.target.value
+			}
+		});
+	};
+
+	handleBody = (event) => {
+		this.setState({
+			postData: {
+				...this.state.postData,
+				postBody: event.target.value
+			}
+		});
 	};
 
 	componentWillMount = async () => {
@@ -28,6 +54,38 @@ export default class EditPost extends Component {
 	};
 
 	render() {
-		return <button onClick={this.handleModal}>Edit</button>;
+		return (
+			<>
+				{this.state.show && (
+					<div className="modal">
+						<button className="close" onCLick={this.handleModal}>
+							X
+						</button>
+						<form
+							className="add-post"
+							onSubmit={(event) => this.handleUpdatePost(event)}
+						>
+							<input
+								style={{ fontSize: '19px' }}
+								type="text"
+								placeholder="Title"
+								name="postTitle"
+								value={this.state.postData.postTitle}
+								onChange={this.handleTitle}
+							/>
+							<input
+								style={{ height: '150px', fontSize: '19px' }}
+								type="text"
+								name="postBody"
+								value={this.state.postData.postBody}
+								onChange={this.handleBody}
+							/>
+							<button>Update Post</button>
+						</form>
+					</div>
+				)}
+				<button onClick={this.handleModal}>Edit</button>
+			</>
+		);
 	}
 }

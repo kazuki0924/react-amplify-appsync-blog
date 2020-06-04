384fdff023c8d75ad84043ea76f8393c4184ce2c
Kazuki Matsuzaki <kazuki0924@gmail.com>
2020-06-05 08:58:41 +0900

    24

--- src/components/UsersWhoLikedPost.js
+++ src/components/UsersWhoLikedPost.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export default class UsersWhoLikedPost extends Component {
-	render() {
-		const allUsers = this.props.data;
-		return allUsers.map((user) => {
-			return (
-				<>
-					<div key={user}>
-						<span style={{ fontStyle: 'bold', color: '#ged' }}>{user}</span>
-					</div>
-				</>
-			);
-		});
-	}
+
+class UsersWhoLikedPost extends Component {
+
+
+    render() {
+        const allUsers = this.props.data
+         return allUsers.map((user) => {
+            return (
+            
+                    <div key={user}>
+                        <span style={{fontStyle:"bold", color: "#ged"}}>
+                             {user} 
+
+                        </span>
+
+                    </div>
+             
+            )
+         })
+
+    }
 }
+export default UsersWhoLikedPost
